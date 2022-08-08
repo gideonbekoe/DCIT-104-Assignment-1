8cbee7b02596fe7f984fc6d26db23323ee6562c4
gideonbekoe <110828985+gideonbekoe@users.noreply.github.com>
2022-08-08 13:10:25 +0000

    initial commit

---
+++ Assignment 1.py
@@ -0,0 +1,33 @@
+primeNum = []
+
+number=int(input("Enter Number: "))
+def isPrime(k):
+    i = 2
+    while (i < k):
+        if k % i == 0:
+            return False
+        i = i + 1
+    return True
+
+
+def printNum(l):
+    x= 2
+    while x <= l:
+        if isPrime(a):
+            primeNum.append(x)
+        x= x + 1
+
+
+printNum(number)
+
+numOfPrime = len(primeNum)
+sum = 0
+
+v = 0
+while v< numOfPrime:
+    sum += primeNum[v]
+    v = v + 1
+
+ans = sum / numOfPrime
+
+print(ans)
